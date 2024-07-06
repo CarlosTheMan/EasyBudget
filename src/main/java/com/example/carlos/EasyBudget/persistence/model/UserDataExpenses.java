9810f763581634f32e992f71e9e62ae6430c7bd1
CarlosTheMan <36449121+CarlosTheMan@users.noreply.github.com>
2024-07-06 17:55:39 -0400

    Update UserDataExpenses model
    -Added private ID variable to store associated id for a DB transaction

--- src/main/java/com/example/carlos/EasyBudget/persistence/model/UserDataExpenses.java
+++ src/main/java/com/example/carlos/EasyBudget/persistence/model/UserDataExpenses.java
@@ -1,14 +1,24 @@
 package com.example.carlos.EasyBudget.persistence.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Expenses")
 public class UserDataExpenses {
 
+    @Id private String id;
     private String userid;
     private String category;
     private String expense;
     private String date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getUserid() {
         return userid;
